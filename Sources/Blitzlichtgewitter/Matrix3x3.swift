95d3ad4df20ab2592b9795e83cfd5f39e0f63f5c
Lennart Stolz <lennartstolz@me.com>
2021-10-21 21:29:13 +0200

    [03] "Computing Cofactors"

--- Sources/Blitzlichtgewitter/Matrix3x3.swift
+++ Sources/Blitzlichtgewitter/Matrix3x3.swift
@@ -38,7 +38,7 @@ extension Matrix3x3 {
 
     /// Returns the minor (the determinant of the submatrix at the given `row` and `column`).
     ///
-    /// - Parameters
+    /// - Parameters:
     ///     - row: The row of the submatrix to determine the minor.
     ///     - column: The column of the submatrix to determine the minor.
     ///
@@ -46,5 +46,43 @@ extension Matrix3x3 {
     public func minor(_ row: Int, _ column: Int) -> Scalar {
         submatrix(row, column).determinant
     }
+
+}
+
+// MARK: Cofactors
+
+extension Matrix3x3 {
+
+
+    /// Returns the cofactor of the given row and column.
+    ///
+    /// - Parameters:
+    ///     - row: The row to determine the cofactor.
+    ///     - column: The column determine the cofactor.
+    ///
+    /// The cofactor of the matrix is a (potentially) negated minor of the 3x3 matrix.
+    /// It is negated for all (`row`/`column`) combinations where their element in the given matrix is `-`.
+    ///
+    /// ```
+    /// [ +  -  + ]
+    /// [ -  +  - ]
+    /// [ +  -  + ]
+    /// ```
+    ///
+    /// - Returns the cofactor of the given row and column.
+    public func cofactor(_ row: Int, _ column: Int) -> Scalar {
+        let minor = minor(row, column)
+        return (row + column).isOdd ? -minor : minor
+    }
+
+}
+
+private extension Int {
+
+    /// Returns true if the value represents an odd number.
+    var isOdd : Bool { !isEven }
+
+    /// Returns true if the value represents an even number.
+    var isEven : Bool { self % 2 == 0 }
 
 }
