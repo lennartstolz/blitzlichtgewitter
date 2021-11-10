5ad89c7b797f23e27b00cb6fec5aed428273f28b
Lennart Stolz <lennartstolz@me.com>
2021-11-10 08:38:52 +0100

    [05] "Transforming Rays and Spheres" (#25)

--- Sources/Blitzlichtgewitter/Sphere.swift
+++ Sources/Blitzlichtgewitter/Sphere.swift
@@ -9,15 +9,20 @@ public struct Sphere {
     /// The spheres radius around the origin.
     public let radius: Double
 
+    /// The transform applied to sphere.
+    public var transform: Matrix4x4
+
     /// Creates a sphere geometry with the specified origin and radius.
     ///
     /// - Parameters:
     ///     - origin: The spheres origin.
     ///     - radius: The spheres radius around the origin.
-    public init(origin: Tuple, radius: Double) {
+    ///     - transform: The transform applied to sphere.
+    public init(origin: Tuple, radius: Double, transform: Matrix4x4 = .identity) {
         assert(origin.isPoint)
         self.origin = origin
         self.radius = radius
+        self.transform = transform
     }
 
 }
