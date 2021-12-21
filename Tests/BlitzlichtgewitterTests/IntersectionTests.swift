4dad865032247e211df00f6951bd6a044e3950aa
Lennart Stolz <lennartstolz@me.com>
2021-12-21 21:53:13 +0100

    [Chore] Convenient way to instantiate a unit sphere added

--- Tests/BlitzlichtgewitterTests/IntersectionTests.swift
+++ Tests/BlitzlichtgewitterTests/IntersectionTests.swift
@@ -4,7 +4,7 @@ import Blitzlichtgewitter
 final class IntersectionTests : XCTestCase {
 
     func testAnIntersectionEncapsulatedTAndObject() {
-        let s = Sphere(origin: point(0, 0, 0), radius: 1)
+        let s: Sphere = .unit
         let i = Intersection(t: 3.5, object: s)
         XCTAssertEqual(i.t, 3.5)
         XCTAssertEqual(i.object, s)
